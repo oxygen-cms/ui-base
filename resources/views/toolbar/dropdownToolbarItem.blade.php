780433c8147559cb8293b8b93745c26ab1f250a6
Chris Chamberlain <c.chamberlain@iinet.net.au>
2014-11-15 09:13:47 +1100

    DropdownToolbarItem primary action

--- resources/views/toolbar/dropdownToolbarItem.blade.php
+++ resources/views/toolbar/dropdownToolbarItem.blade.php
@@ -1,8 +1,28 @@
 <div class="{{{ ($insideMainNav ? 'MainNav-item ' : '') . 'Dropdown-container' }}}">
-    <button class="{{{ $insideMainNav ? 'MainNav-link' : 'Button Button-color--' . $toolbarItem->color . ' Dropdown-button' }}}">
-        {{{ $toolbarItem->label }}}
-        <span class="Icon Icon-{{{ $toolbarItem->icon }}} Icon--pushLeft"></span>
-    </button>
+    <?php
+        $buttonClasses = ['Dropdown-button'];
+        if($insideMainNav) {
+            $buttonClasses[] = 'MainNav-link';
+        } else {
+            $buttonClasses[] = 'Button Button-color--' . $toolbarItem->color;
+        }
+    ?>
+    @if(is_string($toolbarItem->label))
+        <button class="{{{ implode(' ', $buttonClasses) }}}">
+            {{{ $toolbarItem->label }}}
+            <span class="Icon Icon-{{{ $toolbarItem->icon }}} Icon--pushLeft"></span>
+        </button>
+    @else
+        <div class="ButtonTabGroup">
+            <?php
+                echo $toolbarItem->label->render([
+                    'model'          => $model,
+                    'insideMainNav'  => $insideMainNav
+                ]);
+            ?>
+            <span class="Icon Icon-{{{ $toolbarItem->icon }}} Icon--pushLeft Button Button-color--white"></span>
+        </div>
+    @endif
     <div class="{{{ (!$insideMainNav ? 'Dropdown--round ' : '') . 'Dropdown' }}}">
         <?php
             foreach($toolbarItem->itemsToDisplay as $item):
