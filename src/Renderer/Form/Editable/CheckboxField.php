132a24bde744ae6feadad2f9090547decb6547a3
Chris Chamberlain <chris@chamberlain.id.au>
2020-07-25 19:52:04 +1000

    Updated deps and changed input tag display

--- src/Renderer/Form/Editable/CheckboxField.php
+++ src/Renderer/Form/Editable/CheckboxField.php
@@ -2,8 +2,6 @@
 
 namespace Oxygen\UiBase\Renderer\Form\Editable;
 
-use Oxygen\Core\Html\Form\Field;
-
 class CheckboxField extends BaseField {
 
     /**
@@ -24,8 +22,7 @@ class CheckboxField extends BaseField {
 
         return
             $this->getInputTag('hidden', $options['off'], array_merge($attributes, ['id' => null])) .
-            $this->getInputTag('checkbox', $options['on'], array_merge($attributes, ['checked' => $this->isSelected($field, $options['on'])])) .
-            $this->getLabelTag($field->getMeta()->name, $field->getMeta()->label, ['class' => 'Form-checkbox-label flex-item']);
+            $this->getInputTag('checkbox', $options['on'], array_merge($attributes, ['checked' => $this->isSelected($field, $options['on'])]));
     }
 
 }
